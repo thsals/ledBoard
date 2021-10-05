ada8916fd671c5e3d3bd35d9beec8a25cdb2539a
thsals <thsals1111@dgsw.hs.kr>
2021-10-05 16:43:44 +0900

    Initial Commit

---
+++ ledBoard/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ledBoard
+//
+//  Created by 손민 on 2021/10/05.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
